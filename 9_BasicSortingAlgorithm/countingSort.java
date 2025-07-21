8c6a4432f2a2563548ee1b60a1648f340bffbc95
Satyam_Verma <satyamverma260404@gmail.com>
2025-07-21 23:53:46 +0530

    counting Sort

---
+++ 9_BasicSortingAlgorithm/countingSort.java
@@ -0,0 +1,34 @@
+public class countingSort {
+
+    public static void countingSort(int arr[]) {
+        // finding maximum digit for size of count (frequency) array
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        //count (frequency) array
+        int count[] = int new[largest + 1];
+        for(int i = 0; i < arr.length; i++){
+            count[arr[i]]++;
+        }
+        //Shorting count array
+        int j = 0;
+       
+        for(int i = 0 ; i < count.length; i++){
+            while (count[i]>0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+        for(int i = 0 ; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 5, 3, 2, 1, 1, 5, 6, 7 };
+        countingSort(arr);
+    }
+}
